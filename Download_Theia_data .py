e359deaec7bef6db3c8a2c8e3eaf787b57f1cb16
JuniaS-20 <66285080+JuniaS-20@users.noreply.github.com>
2021-05-13 00:34:17 +0200

    Update Download_Theia_data .py

--- Download_Theia_data .py
+++ Download_Theia_data .py
@@ -1,14 +1,36 @@
 # -*- coding: Latin-1 -*-
+#########################
+# Importing modules
+#########################
+
 import os
+
+#  ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ====
+# Definition of the function
+#  ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ====
+
 def Download_Tiles(tile, date_init, date_end, dir_write):
     cli='python theia_download.py -t ' + tile + ' -a config_theia.cfg -d ' + date_init + ' -f ' + date_end + ' -c SENTINEL2  ' + ' -w '+ dir_write
     os.system(cli)
+    
+#  ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ====
+# Declaration and assignment of variables 
+#  ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ====
 
 tiles = ('T30TYQ','T30TYR','T31TCL','T31TCK')
 init_date = '2018-01-01'
 end_date = '2018-12-31'
 dir_out = 'F:/Sentinel2/Dordogne/2017-2018' #F:\Dordogne
+
+#  ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ====
+# Code execution 
+#  ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ====
+
 for tuile in tiles:
     Download_Tiles(tuile,init_date,end_date, dir_out)
+    
+#  ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ====
+# End of the execution
+#  ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ====    
 
 print(' OK Jazz '.center(80, '*'))
